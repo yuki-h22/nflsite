9e01b36e82bf0ec490538b12ec875f0457d09842
yuki-h22 <h-y.e-24-olaf@ezweb.ne.jp>
2021-08-09 01:02:38 +0900

    change2

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   resources :blogs
   root 'blogs#index'
   get 'index' => 'blogs#index'
-  get 'about' => 'blogs#about'
+  get 'application' => 'blogs#application'
   get 'works' => 'blogs#works'
-  get 'link' => 'blogs#link'
+  get 'guidance' => 'blogs#guidance'
   get 'contact' => 'blogs#contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
